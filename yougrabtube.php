17431c8f365602e3e1b6fdd95207ef9c1d9bdc52
selesdepselesnul <selesdepselesnul@gmail.com>
2016-07-06 09:19:04 +0700

    ready to give link feedback

--- yougrabtube.php
+++ yougrabtube.php
@@ -3,11 +3,23 @@
 require __DIR__ . '/vendor/autoload.php';
 use Telegram\Bot\Api;
 
+// author : Moch Deden
+
 $config = parse_ini_file('yougrabtube.ini');
 
 $telegram = new Api($config['api_key']);
 
 $response = $telegram->getUpdates(
   ['offset' => -1, 'limit' => 1, 'timeout' => 0]);
-$update = $response[0];
-print_r($response[0]->getMessage());
+
+$message = $response[0]->getMessage();
+$chat = $message->getChat();
+
+$response = $telegram->sendMessage([
+  'chat_id' => $chat->getId(), 
+  'text' => $message->getText()
+]);
+
+$messageId = $response->getMessageId();
+
+echo $messageId;
