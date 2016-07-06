26b25d2cd228ded00bfdd215e5688bdd78f9027a
selesdepselesnul <selesdepselesnul@gmail.com>
2016-07-06 10:37:53 +0700

    delete unused depedency

--- yougrabtube.php
+++ yougrabtube.php
@@ -1,13 +1,12 @@
 #!/usr/bin/env php
 <?php
+// author : Moch Deden
 require __DIR__ . '/vendor/autoload.php';
 use Telegram\Bot\Api;
 
-// author : Moch Deden
-
 $config = parse_ini_file('yougrabtube.ini');
 
-$telegram = new Api($config['api_key']);
+$telegram = new Api($config['telegram_bot_key']);
 
 $response = $telegram->getUpdates(
   ['offset' => -1, 'limit' => 1, 'timeout' => 0]);
@@ -17,9 +16,10 @@ $chat = $message->getChat();
 
 $response = $telegram->sendMessage([
   'chat_id' => $chat->getId(), 
-  'text' => $message->getText()
+  'text' => 'http://goo.gl/N8VEHY',
 ]);
 
 $messageId = $response->getMessageId();
+
 
 echo $messageId;
