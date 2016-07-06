3fc2e2f86eb1d353a3db88613254d038826a2baf
selesdepselesnul <selesdepselesnul@gmail.com>
2016-07-06 18:50:06 +0700

    add polite invitation to contribute

--- yougrabtube.php
+++ yougrabtube.php
@@ -26,10 +26,10 @@ class YouGrabTube {
     private function makeUrlShort($url) {
         $response = Unirest\Request::get(
           $this->config['shortener_end_point'].$url,
-          array(
+          [
             "X-Mashape-Key" => $this->config['mashape_key'],
             "Accept" => "application/xml"
-          )
+          ]
         );  
         $shortener = simplexml_load_string($response->raw_body);
         $shortUrl = (string)$shortener->ShortUrl;
@@ -81,12 +81,25 @@ class YouGrabTube {
               
                 $this->sendMessage('click the link and klik ok when telegram ask you !');    
             }
-
             
+        } elseif (preg_match('/(please)?(\s)*help(\s)*(me)?/i', $this->message->getText())) {
+            $this->sendMessage(
+              'you are such a polite person !');
+            $this->sendMessage(
+              'ok will help you to download youtube video');
+            $this->sendMessage(
+              'to download youtube video you just need to give');
+            $this->sendMessage(
+              "an youtube url to me, that's it :)");
         } else {
             $this->sendMessage(
               '<b>WTF R U talkin bout ?'
               .PHP_EOL.'do U speak properly, dude ?</b>');
+            $this->sendMessage(
+              'do u want to teach me speak your language ? '
+              .PHP_EOL.'if yes, please contribute to my creator repo => <a href="'.
+              'https://github.com/selesdepselesnul'.'">Moch Deden</a>'
+              .PHP_EOL.'he will glad if you want to contribute :)');
         }
 
     }
