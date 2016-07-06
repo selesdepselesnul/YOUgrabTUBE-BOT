0250867e712641f7bdc8c0d1d2428de0ebfb2533
selesdepselesnul <selesdepselesnul@gmail.com>
2016-07-06 10:51:24 +0700

    change depedency to unirest for doing http request

--- yougrabtube.php
+++ yougrabtube.php
@@ -4,22 +4,31 @@
 require __DIR__ . '/vendor/autoload.php';
 use Telegram\Bot\Api;
 
-$config = parse_ini_file('yougrabtube.ini');
-
-$telegram = new Api($config['telegram_bot_key']);
-
-$response = $telegram->getUpdates(
-  ['offset' => -1, 'limit' => 1, 'timeout' => 0]);
-
-$message = $response[0]->getMessage();
-$chat = $message->getChat();
-
-$response = $telegram->sendMessage([
-  'chat_id' => $chat->getId(), 
-  'text' => 'http://goo.gl/N8VEHY',
-]);
-
-$messageId = $response->getMessageId();
+$response = Unirest\Request::get("https://ytgrabber.p.mashape.com/app/get/vbb_DKn21Ig",
+  array(
+    "X-Mashape-Key" => "S60LBMB0ivmshGLcOVyPhT6KTFITp1jjiszjsnQpNmujBNVPuS",
+    "Accept" => "application/json"
+  )
+);
+$links = $response->body->link;
 
 
-echo $messageId;
+// $config = parse_ini_file('yougrabtube.ini');
+
+// $telegram = new Api($config['telegram_bot_key']);
+
+// $response = $telegram->getUpdates(
+//   ['offset' => -1, 'limit' => 1, 'timeout' => 0]);
+
+// $message = $response[0]->getMessage();
+// $chat = $message->getChat();
+
+// $response = $telegram->sendMessage([
+//   'chat_id' => $chat->getId(), 
+//   'text' => 'http://goo.gl/N8VEHY',
+// ]);
+
+// $messageId = $response->getMessageId();
+
+
+// echo $messageId;
