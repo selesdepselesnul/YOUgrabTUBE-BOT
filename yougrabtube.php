03966fa2a953f810ecf333b6f0df5bcb299fa739
selesdepselesnul <selesdepselesnul@gmail.com>
2016-07-06 18:05:19 +0700

    refactor with class

--- yougrabtube.php
+++ yougrabtube.php
@@ -6,102 +6,80 @@ require_once 'YoutubeLinkGenerator.php';
 require_once 'Config.php';
 use Telegram\Bot\Api;
 
-function makeUrlShort($url) {
-    global $config;
-    $response = Unirest\Request::get(
-      $config['shortener_end_point'].$url,
-      array(
-        "X-Mashape-Key" => $config['mashape_key'],
-        "Accept" => "application/xml"
-      )
-    );  
-    $shortener = simplexml_load_string($response->raw_body);
-    $shortUrl = (string)$shortener->ShortUrl;
-    return $shortUrl;
-}
-// function processMessage() {
-//   $query = parse_url($userMessage)['query'];
-//   parse_str($query, $arrQuery);
-//   $videoId = $arrQuery['v'];
-//   $response = Unirest\Request::get($config['downloader_end_point'].$videoId,
-//     [
-//       "X-Mashape-Key" => $config['mashape_key'],
-//       "Accept" => "application/json"
-//     ]
-//   );
+class YouGrabTube {
 
-//   $links = $response->body->link;
 
-//   if($user->getFirstName() != '' 
-//     && $user->getLastName() != '')
-//     $name = $user->getLastName();
-//   else 
-//     $name = $user->getFirstName();
+    public function __construct($config) {
+        
+        $this->config = $config;
+        $this->telegram = new Api($this->config['telegram_bot_key']);
 
-//   $response = $telegram->sendMessage([
-//         'chat_id' => $chat->getId(), 
-//         'text' => 'These are the links i could give to you, '.$name.', enjoy !',
-//   ]);
-//   for ($i=0; $i < count($links)-1 ; $i++) { 
-//     $link = $links[$i];
-//     $response = Unirest\Request::get(
-//         $config['shortener_end_point'].$link->url,
-//         array(
-//           "X-Mashape-Key" => $config['mashape_key'],
-//           "Accept" => "application/xml"
-//         )
-//       );
+        $response = $this->telegram->getUpdates(
+          ['offset' => -1, 'limit' => 1, 'timeout' => 0]);
+
+        $message = $response[0]->getMessage();
+        $this->user = $message->getFrom();
+        $this->chat = $message->getChat();
+            
+        $this->downloadLinks = YoutubeLinkGenerator::generate(
+                        $this->config['mashape_key'], 
+                        $message->getText());   
+
+    }
+
+    private function makeUrlShort($url) {
+        $response = Unirest\Request::get(
+          $this->config['shortener_end_point'].$url,
+          array(
+            "X-Mashape-Key" => $this->config['mashape_key'],
+            "Accept" => "application/xml"
+          )
+        );  
+        $shortener = simplexml_load_string($response->raw_body);
+        $shortUrl = (string)$shortener->ShortUrl;
+        return $shortUrl;
+    }
+
+    private function getNickName() {
+        if($this->user->getFirstName() != '' 
+          && $this->user->getLastName() != '')
+          return $this->user->getLastName();
+        
+        return $this->user->getFirstName();
+    }
+
+
+    private function sendMessage($message) {
+        $this->telegram->sendMessage([
+          'chat_id' => $this->chat->getId(),
+          'parse_mode' => 'HTML', 
+          'text' => PHP_EOL
+              .$message
+              .PHP_EOL]);
+    }
+
+    private function sendDownloadLinks() {
+        foreach ($this->downloadLinks as $i => $downloadLink) {
+            $this->sendMessage('<b>'.($i + 1).'. </b><a href="'
+                  .$this->makeUrlShort($downloadLink['url']).'">'
+                  .$downloadLink['format'].' '.$downloadLink['quality']
+                  .'</a>');
+        }
+    }
+
+    public function start() {
+        
+        $this->sendMessage(
+          'Ok '.$this->getNickName()
+          .', here i give u some links to download the video :');
+
+        $this->sendDownloadLinks();
       
-//       var_dump($link);
-//       $type = $link->type;
-//       $format = $type->format;
-//       $quality = $type->quality;
-//       $shortener = simplexml_load_string($response->raw_body);
-//       $shortUrl = (string)$shortener->ShortUrl;
-       
-    
-//       $response = $telegram->sendMessage([
-//         'chat_id' => $chat->getId(), 
-//         'text' => "format : $format, quality : $quality, dl-url: $shortUrl",
-//       ]);
+        $this->sendMessage('click the link and klik ok when telegram ask you !');
 
-//       $messageId = $response->getMessageId();
-
-//       echo $messageId;
-//   }
-
-//   $response = $telegram->sendMessage([
-//         'chat_id' => $chat->getId(), 
-//         'text' => "open those links in new tab, then right click and click save as !",
-//   ]);  
-// }
-
-
-$telegram = new Api($config['telegram_bot_key']);
-
-$response = $telegram->getUpdates(
-  ['offset' => -1, 'limit' => 1, 'timeout' => 0]);
-
-
-$message = $response[0]->getMessage();
-$user = $message->getFrom();
-$chat = $message->getChat();
-$userMessage = $message->getText();
-
-var_dump($userMessage);
-    
-$downloadLinks = YoutubeLinkGenerator::generate($config['mashape_key'], 
-                                                        $userMessage);
-
-foreach ($downloadLinks as $i => $downloadLink) {
-    $telegram->sendMessage([
-      'chat_id' => $chat->getId(),
-      'parse_mode' => 'HTML', 
-      'text' => PHP_EOL
-          .'<b>'.($i + 1).'. </b><a href="'.makeUrlShort($downloadLink['url']).'">'
-          .$downloadLink['format'].' '.$downloadLink['quality']
-          .'</a>'.PHP_EOL
-    ]);      
+    }
 }
 
+$youGrabTube = new YouGrabTube($config);
+$youGrabTube->start();
 
