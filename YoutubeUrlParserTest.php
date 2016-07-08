917934538d9c3411832c3d7d6a3ced7dbc5d7d43
selesdepselesnul <selesdepselesnul@gmail.com>
2016-07-08 08:47:13 +0700

    add parse long youtube url

--- YoutubeUrlParserTest.php
+++ YoutubeUrlParserTest.php
@@ -3,9 +3,20 @@ require __DIR__ . '/vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 
 class YoutubeUrlParserTest extends TestCase {
+
+	public function setUp() {
+		$this->youtubeUrlParser = new YoutubeUrlParser;
+	}
+
 	public function testParseShortUrl() {
-		$yUrlParser = new YoutubeUrlParser;
+		
 		$this->assertEquals('4OmL8fl_nOo', 
-			$yUrlParser->parseShort('https://youtu.be/4OmL8fl_nOo'));
+			$this->youtubeUrlParser->parseShort('https://youtu.be/4OmL8fl_nOo'));
+	}
+
+	public function testParseLongUrl() {
+		
+		$this->assertEquals('4OmL8fl_nOo', 
+			$this->youtubeUrlParser->parseLong('https://www.youtube.com/watch?v=4OmL8fl_nOo'));
 	}
 }
--- lib/YoutubeUrlParser.php
+++ lib/YoutubeUrlParser.php
@@ -1,7 +1,17 @@
 <?php
 class YoutubeUrlParser {
+	
+	const SHORT_URL = 'https://youtu.be/';
+
 	public function parseShort($url) {
-		$exp = explode('https://youtu.be/', $url);
+		$exp = explode(self::SHORT_URL, $url);
 		return $exp[1];
+	}
+
+	public function parseLong($url) {
+		$query = parse_url($url)['query'];
+		parse_str($query, $arrQuery);
+		$videoId = $arrQuery['v'];
+		return $videoId;
 	}
 }
