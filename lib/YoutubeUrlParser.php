5e21bb7de0dea09559ec98ffee83be2c583d11eb
selesdepselesnul <selesdepselesnul@gmail.com>
2016-07-08 09:09:18 +0700

    add comment to each mehtod of YoutubeUrlParser

--- lib/YoutubeUrlParser.php
+++ lib/YoutubeUrlParser.php
@@ -4,6 +4,7 @@ class YoutubeUrlParser {
 	const SHORT_URL = '/https:\/\/youtu\.be\/.+/i';
 	const LONG_URL = '/https:\/\/www\.youtube\.com\/watch\?v=.+/i';
 
+	// parse short url ex : https://youtu.be/4OmL8fl_nOo
 	public function parseShort($url) {
 		if(preg_match(self::SHORT_URL, $url)) {
 			$path = parse_url($url)['path'];
@@ -13,6 +14,7 @@ class YoutubeUrlParser {
 		return '';
 	}
 
+	// parse long url ex : https://www.youtube.com/watch?v=4OmL8fl_nOo
 	public function parseLong($url) {
 		if(preg_match(self::LONG_URL, $url)) {
 			$query = parse_url($url)['query'];
